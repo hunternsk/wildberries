265d0d0e97edfc37b8e9adae4629f877719e3f06
MaxonClaxon <whimax86@gmail.com>
2022-04-23 17:27:02 +0300

    add OrderId to sale

--- types/sale.go
+++ types/sale.go
@@ -5,17 +5,18 @@ package types
 // Формула вычисления pricewithdisc:
 // Pricewithdisc = totalprice*((100 – discountPercent)/100 ) *((100 – promoCodeDiscount)/100 ) *((100 – spp)/100 )
 type Sale struct {
-	Number            string          `json:"number"`            // Номер документа
-	Date              WildberriesTime `json:"date"`              // Дата продажи
-	LastChangeDate    WildberriesTime `json:"lastChangeDate"`    // Дата и время последнего обновления информации отчёта в сервисе
-	SupplierArticle   string          `json:"supplierArticle"`   // Артикул товара поставщика
-	TechSize          string          `json:"techSize"`          // Технический размер
-	Barcode           string          `json:"barcode"`           // Штрихкод
-	Quantity          int64           `json:"quantity"`          // Количество, если меньше 0 - возврат
-	TotalPrice        float64         `json:"totalPrice"`        // Цена товара из УПД
-	DiscountPercent   float64         `json:"discountPercent"`   // Согласованная итоговая скидка в процентах
-	IsSupply          bool            `json:"isSupply"`          // Договор поставки
-	IsRealization     bool            `json:"isRealization"`     // Договор реализации
+	Number            string          `json:"number"`          // Номер документа
+	Date              WildberriesTime `json:"date"`            // Дата продажи
+	LastChangeDate    WildberriesTime `json:"lastChangeDate"`  // Дата и время последнего обновления информации отчёта в сервисе
+	SupplierArticle   string          `json:"supplierArticle"` // Артикул товара поставщика
+	TechSize          string          `json:"techSize"`        // Технический размер
+	Barcode           string          `json:"barcode"`         // Штрихкод
+	Quantity          int64           `json:"quantity"`        // Количество, если меньше 0 - возврат
+	TotalPrice        float64         `json:"totalPrice"`      // Цена товара из УПД
+	DiscountPercent   float64         `json:"discountPercent"` // Согласованная итоговая скидка в процентах
+	IsSupply          bool            `json:"isSupply"`        // Договор поставки
+	IsRealization     bool            `json:"isRealization"`   // Договор реализации
+	OrderID           uint64          `json:"order_id"`
 	PromoCodeDiscount float64         `json:"promoCodeDiscount"` // Согласованная скидка по промо коду
 	WarehouseName     string          `json:"warehouseName"`     // Название склада отгрузки товара
 	CountryName       string          `json:"countryName"`       // Страна
